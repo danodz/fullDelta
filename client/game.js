1cddeb92bfc671c272702ffa777fe3cf05f0fd9c
Mighty Erwan <erwan.bourdeau@gmail.com>
2016-09-29 23:14:48 +0000

    Basic client server communication structure.

--- client/game.js
+++ client/game.js
@@ -2,4 +2,10 @@ document.addEventListener("DOMContentLoaded",init);
 
 function init() {
     conn = new WebSocket("ws://0.0.0.0:9000");
+    conn.onopen = function (event) {
+        conn.send("Hello"); 
+    };
+    conn.onmessage = function (event) {
+        console.log(event.data);
+    }
 }
--- server/Main.hs
+++ server/Main.hs
@@ -1,19 +1,56 @@
 module Main where
 import qualified Network.WebSockets as WS
+import qualified Data.ByteString.Lazy.Char8 as BS8
+import Control.Concurrent (forkIO, MVar, newMVar, takeMVar, putMVar, modifyMVar_, readMVar, threadDelay)
 
-app :: WS.ServerApp
-app pending = do
+instance Show (MVar a) where
+    show x = "MVar"
+
+data Ship = Ship { x :: Float
+                 , y :: Float
+                 , angle :: Float
+                 , speed :: Float
+                 }
+    deriving (Show)
+
+data ShipCommand = ShipCommand { setX :: Float
+                               }
+
+data GameState = GameState { ship :: Ship
+                           }
+    deriving (Show)
+
+data GameStateCommand = SetX
+
+app :: MVar [GameStateCommand] -> WS.ServerApp
+app gameStateCommands pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
-    read conn
+    WS.sendDataMessage conn $ WS.Text $ BS8.pack "Hello"
+
+    read conn gameStateCommands
   where
-    read conn = do
+    read conn commands = do
         msg <- WS.receiveDataMessage conn
         print msg
-        read conn
+        modifyMVar_ commands (\x -> return $ SetX : x)
+        read conn commands
+
+update :: MVar [GameStateCommand] -> GameState -> IO ()
+update gameStateCommands gameState = do
+    commands <- readMVar gameStateCommands
+    mapM_ (\command -> case command of
+        SetX -> print 0
+      ) commands
+    threadDelay 33333
+    update gameStateCommands gameState
 
 main :: IO ()
 main = do
     putStrLn "Running fullDelta server"
     putStrLn "Welcome to the shit!"
-    WS.runServer "0.0.0.0" 9000 app
+    commands <- newMVar []
+    forkIO $ WS.runServer "0.0.0.0" 9000 $ app commands
+    forkIO $ update commands $ GameState $ Ship 0 0 0 0
+    getLine
+    return ()
