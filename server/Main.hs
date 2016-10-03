82199ce8dc8982c488910eaa208ec65549826326
Mighty Erwan <erwan.bourdeau@gmail.com>
2016-10-03 16:57:55 +0000

    Structure for parsing client message.

--- server/Main.hs
+++ server/Main.hs
@@ -2,6 +2,8 @@ module Main where
 import qualified Network.WebSockets as WS
 import qualified Data.ByteString.Lazy.Char8 as BS8
 import Control.Concurrent (forkIO, MVar, newMVar, takeMVar, putMVar, modifyMVar_, readMVar, threadDelay)
+import qualified Data.Aeson as AE
+import Data.Maybe
 
 instance Show (MVar a) where
     show x = "MVar"
@@ -13,35 +15,33 @@ data Ship = Ship { x :: Float
                  }
     deriving (Show)
 
-data ShipCommand = ShipCommand { setX :: Float
-                               }
-
 data GameState = GameState { ship :: Ship
                            }
     deriving (Show)
 
-data GameStateCommand = SetX
-
-app :: MVar [GameStateCommand] -> WS.ServerApp
+app :: MVar [[String]] -> WS.ServerApp
 app gameStateCommands pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
     WS.sendDataMessage conn $ WS.Text $ BS8.pack "Hello"
 
-    read conn gameStateCommands
+    readMsg conn gameStateCommands
   where
-    read conn commands = do
-        msg <- WS.receiveDataMessage conn
-        print msg
-        modifyMVar_ commands (\x -> return $ SetX : x)
-        read conn commands
+    readMsg conn commands = do
+        msg <- (WS.receiveData conn) :: IO (BS8.ByteString)
+        modifyMVar_ commands (\x ->
+            return $ fromMaybe [""] ((AE.decode msg) :: Maybe [String]) : x)
+        readMsg conn commands
 
-update :: MVar [GameStateCommand] -> GameState -> IO ()
+update :: MVar [[String]] -> GameState -> IO ()
 update gameStateCommands gameState = do
     commands <- readMVar gameStateCommands
-    mapM_ (\command -> case command of
-        SetX -> print 0
+    mapM_ (\command -> case head command of
+        "SetAngle" -> print (( read $ last command ) :: Float)
+        "SetSpeed" -> print (( read $ last command ) :: Float)
+        _ -> print 2
       ) commands
+    modifyMVar_ gameStateCommands (\x -> return [])
     threadDelay 33333
     update gameStateCommands gameState
 
